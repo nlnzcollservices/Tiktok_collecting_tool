9dad85a34b0d38b645830bc9017187b3affe0233
Svetlana Koroteeva <Svetlana.Koroteeva@dia.govt.nz>
2024-07-24 16:59:40 +1200

    Update tiktok collector

---
+++ tests/test_downloader.py
@@ -0,0 +1,66 @@
+import os
+import pytest
+from unittest.mock import patch, MagicMock
+
+# Import the function to be tested
+def download_video(link, video_path, video_folder):
+    import yt_dlp
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'ignoreerrors': True,
+        'writedescription': True,
+        'nocheckcertificate': True,
+        'writecomments': True,
+        'writedescription': True,
+        'writeinfojson': True,
+        'mergeoutputformat': 'mp4',
+    }
+    print("here!!!")
+    print(link)
+    print(video_path)
+    print(video_folder)
+    if not os.path.exists(video_folder):
+        os.makedirs(video_folder)
+    try:
+        print("here")
+        ydl_opts['outtmpl'] = video_path
+        print(ydl_opts)
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        ydl.download([link])
+        flag = True
+    except Exception as e:
+        print(str(e))
+        flag = False
+    return flag
+
+# Test case
+@pytest.mark.parametrize("link, video_path, video_folder, expected", [
+    ("your_link_to_video", "7266307715595586818", "TIKTOK", True),
+])
+def test_download_video(link, video_path, video_folder, expected):
+    with patch("os.makedirs") as mock_makedirs, \
+         patch("os.path.exists", return_value=False), \
+         patch("yt_dlp.YoutubeDL") as mock_yt_dlp:
+        
+        mock_ydl_instance = MagicMock()
+        mock_yt_dlp.return_value = mock_ydl_instance
+        mock_ydl_instance.download.return_value = None  # Simulate successful download
+        
+        result = download_video(link, video_path, video_folder)
+        
+        mock_makedirs.assert_called_once_with(video_folder)
+        mock_yt_dlp.assert_called_once()
+        mock_ydl_instance.download.assert_called_once_with([link])
+        
+        assert result == expected
+
+if __name__ == "__main__":
+    # Get the current script directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Change the working directory to the test directory (assuming 'tests' folder is in the current directory)
+    test_dir = os.path.join(current_dir)
+    
+    # Run the tests in the 'tests' directory
+    pytest.main([test_dir])
--- tiktok_gui_tool.py
+++ tiktok_gui_tool.py
@@ -1,3 +1,4 @@
+
 import subprocess
 import configparser
 import os
@@ -9,8 +10,6 @@ from time import sleep
 from datetime import datetime as dt
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-import requests
-from urllib.request import urlopen
 import yt_dlp
 import PySimpleGUI as sg
 import urllib3
@@ -28,26 +27,26 @@ ydl_opts = {
     'writeinfojson': True,
     'mergeoutputformat': 'mp4',
 }
+def is_valid_url(url):
+    """Check if the provided URL is valid."""
+    try:
+        result = requests.get(url, verify=False)
+        return result.status_code == 200
+    except:
+        return False
 
 def my_gui():
-    form = sg.FlexForm('form name')
     scheme = "LightGreen2"
     sg.theme(scheme)
-    value_list = ["value1", "value2"]
     layout = [
-
-            [sg.Text('Insert link to tiktok web page',font = ('Helvetica', 13, 'bold italic'))],
-            [sg.Text('Full path with tag or username',font = ('Helvetica', 11, 'italic'))],
-            [sg.Text('Tiktok link', size=(10, 1)), sg.InputText("",key='url',size=(100, 1))],  # Removed the default link
-            [sg.Text('', size=(100, 1)),sg.FolderBrowse('FolderBrowse',key='foldername')],
-            [sg.Button("Run!")]
-
-        ]
-    window = sg.Window(f'TikTok downloader', layout, default_element_size=(35, 2))
-    event, values = window.read()
-
-    return values, window, event
-
+        [sg.Text('Insert link to tiktok web page', font=('Helvetica', 13, 'bold italic'))],
+        [sg.Text('Full path with tag or username', font=('Helvetica', 11, 'italic'))],
+        [sg.Text('Tiktok link', size=(10, 1)), sg.InputText("", key='url', size=(100, 1))],
+        [sg.Text('', size=(100, 1)), sg.FolderBrowse('FolderBrowse', key='foldername')],
+        [sg.Button("Run!"), sg.Button("Quit")]
+    ]
+    window = sg.Window('TikTok downloader', layout, default_element_size=(35, 2))
+    return window
 
 def is_single_video(url):
     """Check if the provided URL is a single video link."""
@@ -60,61 +59,64 @@ def get_tiktok_videos(url, storage_folder):
     """
     flag_collected = False
     print(url)
-    items = []
-    driver = webdriver.Firefox()
-    print("here1")
-    if not os.path.exists(storage_folder):
-        os.makedirs(storage_folder)
-    downloaded_files = os.listdir(storage_folder)
-    print("here2")
-    driver.set_window_size(768, 1024)
-    driver.get(url)
-    driver.maximize_window()
-    sleep(2)
-    driver.get(url)
-    sleep(15)
 
     if is_single_video(url):
         # If it's a single video link, download that video
-        video_link = url
+        print("Single url")
+        video_link = str(url)
         video_id = url.split("/")[-1]
-        video_collector([video_link], storage_folder, {video_link: video_id}, downloaded_files)
+        author = url.split("/")[-3].lstrip("@")
+        video_title = author +"_" +video_id
+        flag_collected = download_video(video_link, video_title, storage_folder)
     else:
         # If it's a profile link, collect and download all videos
+        items = []
+        driver = webdriver.Firefox()
+        print("Driver activated")
+        if not os.path.exists(storage_folder):
+            os.makedirs(storage_folder)
+        downloaded_files = os.listdir(storage_folder)
+        print("Already downloaded")
+        print(downloaded_files)
+        driver.set_window_size(768, 1024)
+        driver.get(url)
+        driver.maximize_window()
+        sleep(2)
+        driver.get(url)
+        sleep(15)
+
         scroll_down(driver)
         my_links = []
         my_links_dict = {}
-        print("here5")
         soup = bs(driver.page_source, 'html.parser')
         print(soup.text)
         items = []
         itms_titles = {}
         lnks_soup = soup.find_all("div", attrs={"class": "css-at0k0c-DivWrapper e1cg0wnj1"})
-        for lnk_soup in lnks_soup:
-            lnk = lnk_soup.find("a").attrs["href"]
-            print(lnk)
-            items.append(lnk)
-            title = lnk_soup.find("img").attrs["alt"]
-            print(title)
-            itms_titles[lnk] = title
+        if not lnks_soup:
+            sg.Print("Failed. Either No URLs found in the TikTok profile or run again and do not forget to 'refresh' or/and solve cute Tiktok puzzle in your FireFox instance.")
+            flag_collected=False
+        else:
+            for lnk_soup in lnks_soup:
+                lnk = lnk_soup.find("a").attrs["href"]
+                items.append(lnk)
+                title = lnk_soup.find("img").attrs["alt"]
+                itms_titles[lnk] = title
 
-        video_collector(items, storage_folder, itms_titles, downloaded_files)
-        file_collected = True
+            video_collector(items, storage_folder, itms_titles, downloaded_files)
+            flag_collected = True
+
     return flag_collected
 
-
-
 def scroll_down(driver):
-
     """
     A method for scrolling the page.
     Origin: #https://stackoverflow.com/questions/48850974/selenium-scroll-to-end-of-page-in-dynamically-loading-webpage
     """
-
     last_height = driver.execute_script("return document.body.scrollHeight")
-
+    sg.Print("Scrolling...")
+    print("Scrol_down")
     while True:
-
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         sleep(6)
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -122,71 +124,84 @@ def scroll_down(driver):
             break
         last_height = new_height
 
-def download_video(link,  video_path, video_folder):
-	print("here!!!")
-	print(link)
-	print(video_path)
-	print(video_folder)
-	if not os.path.exists(video_folder):
-		os.makedirs(video_folder)
-	try:
-		ydl_opts['outtmpl']=video_path
-		print(ydl_opts)
-		ydl = yt_dlp.YoutubeDL(ydl_opts)
-		ydl.download([link])	
-		flag = True
-	except:
-		flag = False
-	return flag
+def download_video(link, video_path, video_folder):
+    print("Downloading")
+    print(link)
+    print(video_path)
+    print(video_folder)
+    if not os.path.exists(video_folder):
+        os.makedirs(video_folder)
+    try:
+        ydl_opts['outtmpl'] = video_path
+        print(ydl_opts)
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        ydl.download([link])
+        flag = True
+    except:
+        flag = False
+    return flag
 
+def parse_link(storage_folder, video_id):
+    video_path = os.path.join(storage_folder, video_id, video_id + ".mp4")
+    video_folder = os.path.join(storage_folder, video_id)
+    return video_path, video_folder
 
-def parse_link(storage_folder,video_id):
+def video_collector(items, storage_folder, itms_titles, downloaded_files):
+    """Gets list of video links and then downloads each video. Set item.flag = True if complete."""
+    print("Videos")
+    #print(os.getcwd())
 
-	video_path =  os.path.join(storage_folder, video_id, video_id+".mp4")
-	video_folder = os.path.join(storage_folder, video_id )
-	return(video_path, video_folder)
+    if not os.path.exists(storage_folder):
+        os.makedirs(storage_folder)
+    downloaded_files = os.listdir(storage_folder)
+    print(items)
+    for itm in items:
+        print(itm)
+        sg.Print("Collecting " + itm)
+        video_id = itm.split("/")[-3].lstrip("@") + "_" + itm.split("/")[-1]
+        if not video_id in downloaded_files:
+            download_path, download_folder = parse_link(storage_folder, video_id)
+            flag = download_video(itm, download_path, download_folder)
+            if not os.path.exists(download_path) or os.path.getsize(download_path) == 0:
+                flag = download_video(itm, download_path, download_folder)
+            if flag:
+                with open(os.path.join(storage_folder, "links_processed.txt"), "a") as f:
+                    f.write(itm)
+                    f.write("\n")
+                with open(os.path.join(storage_folder, "links_titles.txt"), "a", encoding="UTF-8") as f:
+                    f.write(itm + "|||" + itms_titles[itm])
+                    f.write("\n")
+        else:
+            print("Video exists")
+            sg.Print("Video exists")
+            flag= True
 
-def video_collector(items, storage_folder, itms_titles, downladed_files ):
-
-	"""Gets list of video links and then downloads each video. Set item.flag = True if complete."""
-
-	print("Videos")
-	print(os.getcwd())
-
-	if not os.path.exists(storage_folder):
-		os.makedirs(storage_folder)
-	downloaded_files = os.listdir(storage_folder)
-
-
-	for itm in items:
-		print(itm)
-		sg.Print("Collecting " + itm)
-		video_id = itm.split("/")[-3].lstrip("@")+"_"+itm.split("/")[-1]
-		if not video_id in downloaded_files:
-			download_path, download_folder = parse_link(storage_folder, video_id)
-			flag = download_video(itm,  download_path,download_folder)
-			if not os.path.exists(download_path) or os.path.getsize(download_path)==0:
-				flag = download_video(itm,  download_path, download_folder)
-			if  flag:
-				with open(os.path.join(storage_folder,"links_processed.txt"),"a") as f:
-						f.write(itm)
-						f.write("\n")
-				with open(os.path.join(storage_folder,"links_titles.txt"), "a",encoding="UTF-8") as f:
-						f.write(itm +"|||"+itms_titles[itm])
-						f.write("\n")
-
-	completed_flag = flag
-
-	return flag
+    completed_flag = flag
+    return flag
 
 def main():
-    values, window, event = my_gui()
-    foldername = values["foldername"]
-    url = values["url"]  # Get the TikTok link from user input
-    sg.Print('Collecting from ', url)
-    get_tiktok_videos(url, foldername)  # Use the user-provided link
-    sg.Print("Done!")
-    window.close()
+    while True:
+        window = my_gui()
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit', 'Quit'):
+            window.close()
+            break
+        foldername = values.get("foldername")
+        url = values.get("url")
+        
+        if not foldername or not url:
+            sg.Print("No folder or link provided")
+        elif not is_valid_url(url):
+            sg.Print("Invalid URL provided")
+
+        else:
+            try:
+                sg.Print('Collecting from ', url)
+                get_tiktok_videos(url, foldername)
+                sg.Print("Done.")
+            except Exception as e:
+                sg.Print(f"An error occurred: {e}")
+        window.close()
 
 if __name__ == '__main__':
     main()
